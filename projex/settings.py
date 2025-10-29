f03d7bd6da76836a8f9a8ff133d011b6b02f869d
Siya Lalwani <siyalalwani.dcs@gmail.com>
2025-10-29 15:27:24 +0530

    Allow all hosts for Render

--- projex/settings.py
+++ projex/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-o+yz+tuh0#p!x2tsw)4(n=p-+le!vfs39h_60^!^-ygqvqs=m^
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['projex-i3o1.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 ALLOWED_HOSTS = []
